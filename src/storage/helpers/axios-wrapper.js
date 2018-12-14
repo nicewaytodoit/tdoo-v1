5ece47433843ed15893f7b17f8e7e173ede22340
Aleksandar Ristevski <nice@ristevski.me>
2018-12-14 21:32:11 +0000

    Remove comments

--- src/storage/helpers/axios-wrapper.js
+++ src/storage/helpers/axios-wrapper.js
@@ -1,12 +1,5 @@
 import axios from 'axios';
 
-
-console.log('@@@ Hajde1:', process.env.NODE_ENV);
-console.log('@@@ Hajde2:', process.env.REACT_APP_API_URL);
-console.log('@@@ Hajde3:', process.env.REACT_APP_SECRET_CODE);
-console.log('@@@ Hajde4:', process.env.REACT_APP_URLS);
-console.log('@@@ Hajde5:', process.env.REACT_APP_BLAH);
-
 const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     headers: {
